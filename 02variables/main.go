384fef688dc870c20f4b13706910e2a423d93074
Shu12388y <spl.sp999@gmail.com>
2024-06-15 21:43:32 +0530

    add the variable folder

---
+++ 02variables/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+
+	// string
+	var username string = "shubham"
+	fmt.Println(username)
+	fmt.Printf("the type of username is %T \n ", username)
+
+
+	// boolean
+	var isLogged bool = true
+	fmt.Println(isLogged)
+	fmt.Printf("The type of isLogged is %T", isLogged)
+
+	// integer
+	var number int = 234
+	fmt.Println(number)
+	fmt.Printf("The type of number is %T", number)
+
+
+	// floating
+	var floating float32 = 4.34
+	fmt.Println(floating)
+
+	
+	// declare the variable without var keyword
+	jwt := "ndjsnfnnfkf"
+	fmt.Println(jwt)
+	fmt.Printf("The type of jwt is %T",jwt)
+
+}
